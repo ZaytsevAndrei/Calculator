4a92b74b266dd78706a1490bd7dcbe11a2d250d3
Андрей Зайцев <katote88@gmail.com>
2022-01-13 12:22:49 +0300

    add  recursion project

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,27 +4,51 @@ namespace Calculator
 {
     class Program
     {
+        private static double firstNum;
+        private static double secondNum;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Добро пожаловать в калькулятор простых чисел!" +
-                "\nДля запуска необходимо следовать инструкции и нажать Enter после каждого пункта:" +
-                "\n1. Выбрать предложенную операцию +, -, / или * " +
-                "\n2. Ввести первую цифру" +
-                "\n3. Ввести вторую цифру");
-            
-            Console.WriteLine("\nВыберите арифметическую операцию, для этого введите на клавиатуре на выбор:  +, -, / или * ");
+            try
+            {
+                Calculate();
+            }
+            catch (Exception e)
+            {
+                RestartOnException(e);
+            }
+        }
+
+        private static void RestartOnException(Exception e)
+        {
+            Console.WriteLine("Во время выполения произошла ошибка: {0}", e.Message);
+            Console.WriteLine("Нажмите любую клавишу, чтобы перезапустить программу");
+            Console.ReadKey();
+            Calculate();
+        }
+
+         
+        private static void Calculate()
+        {
+            Console.WriteLine("\nДобро пожаловать в калькулятор простых чисел!" +
+                            "\nДля запуска необходимо следовать инструкции и нажать Enter после каждого пункта:" +
+                            "\n1. Выбрать одну из операций +, -, / или * " +
+                            "\n2. Ввести первую цифру" +
+                            "\n3. Ввести вторую цифру");
+
+            Console.WriteLine("\nВыберите арифметическую операцию");
             string flag = Console.ReadLine();
             Console.WriteLine("Введите первую цифру");
-            int firstNum = int.Parse(Console.ReadLine());
+            firstNum = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Введите вторую цифру");
-            int secondNum = int.Parse(Console.ReadLine());
+            secondNum = Convert.ToDouble(Console.ReadLine());
 
-            var result = 0;
+            double result = 0;
             if (flag == "+")
             {
                 result = firstNum + secondNum;
-            }    
-            
+            }
+
             else if (flag == "-")
             {
                 result = firstNum - secondNum;
@@ -44,67 +68,9 @@ namespace Calculator
                 Console.WriteLine("Выбрана неправильная операция ...");
             }
 
-            Console.WriteLine("{0}{1}{2}={3}", firstNum, flag,  secondNum, result);
+            Console.WriteLine("{0}{1}{2}={3}", firstNum, flag, secondNum, result);
 
             Console.ReadLine();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            /*int num1 = int.Parse(Console.ReadLine());
-            string sign = Console.ReadLine();
-            int num2 = int.Parse(Console.ReadLine());
-
-            var result = 0;
-            if (sign == "+")
-            {
-                result = num1 + num2;
-            }
-            else if (sign == "-")
-            {
-                result = num1 - num2;
-            }
-            else if (sign == "*")
-            {
-                result = num1 * num2;
-            }
-            else if (sign == "/")
-            {
-                result = num1 / num2;
-            }
-            else
-            {
-                Console.WriteLine("Wrong operation sign ...");
-            }
-
-            Console.WriteLine("{0}{1}{2}={3}", num1, sign, num2, result);
-
-            Console.ReadLine();*/
-
-
-
         }
-
-
     }
-
-
 }
-
